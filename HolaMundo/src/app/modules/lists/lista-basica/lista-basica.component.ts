310c2660c09c641b3a0aadb734c8765d6e3c4b6a
SheylaPDV <92029579+SheylaPDV@users.noreply.github.com>
2022-09-28 12:24:00 +0200

    modulos, directivas, ngstyle,ngclass

--- HolaMundo/src/app/modules/lists/lista-basica/lista-basica.component.ts
+++ HolaMundo/src/app/modules/lists/lista-basica/lista-basica.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+// creamos un tipo base para el ejemplo
+export type Producto = {
+  nombre: String;
+  precio: number;
+  descripcion: String;
+};
+
 @Component({
   selector: 'app-lista-basica',
   templateUrl: './lista-basica.component.html',
-  styleUrls: ['./lista-basica.component.css']
+  styleUrls: ['./lista-basica.component.css'],
 })
 export class ListaBasicaComponent implements OnInit {
+  listaElementos: Producto[] = [
+    {
+      nombre: 'leche',
+      precio: 10,
+      descripcion: 'Leche entera',
+    },
+    {
+      nombre: 'Carne',
+      precio: 20,
+      descripcion: 'Carne de cerdo',
+    },
+  ];
+  cargando: boolean = true;
 
-  constructor() { }
+  opcion: number = 0;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  cambiarCargando() {
+    this.cargando = !this.cargando;
   }
 
+  escogerOpcion(opcionEscogida: number) {
+    this.opcion = opcionEscogida; //El valor cambia, implica un cambio en los elementos renderizados
+  }
 }
